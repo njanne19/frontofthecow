58ee50d6d6f579c433b3e80b7d82257548f6c157
Nick Janne <njanne@umich.edu>
2024-08-19 09:40:08 -0400

    Fixed cowspot anim

--- app/components/MetaBallsAttempt2.tsx
+++ app/components/MetaBallsAttempt2.tsx
@@ -1,6 +1,6 @@
 'use client'; 
 
-import React, { useState, useEffect, use } from 'react' 
+import React, { useState, useEffect } from 'react' 
 import styles from './MetaBalls.module.css'; 
 import { Properties } from 'csstype';
 
@@ -9,80 +9,46 @@ import { Properties } from 'csstype';
 // Generator props are passed into the parent function and describe
 // how metaballs should be initially generated. After this, 
 // then each metaball has its own props (see below)
-interface MetaballGeneratorProps {
+export interface MetaballGeneratorProps {
     filenames: string[], // Files to use as cowspots
     numMetaballs: number, // Total number of balls
     generationOrder: string // How to sample cowspot svg files, default ('random') 
 }
 
 // Define interface for a single metaball 
-interface MetaballProps {
+export interface MetaballProps {
     spotFile: string,
     left: number, 
     top: number, 
     size: number, 
     initialLeft: number, 
     initialTop: number, 
-    velocity: number
-}
-
-// Define a function that generates initial metaball props
-// This is called in the parent page to ensure that props are agreed upon 
-// between server and client 
-function generateInitialMetaballs(props: MetaballGeneratorProps): MetaballProps[] {
-
-    let metaballPropsList: MetaballProps[] = []; 
-
-    // Decompose the props, generate a metaball for each. 
-    for (let i = 0; i < props.numMetaballs; i++) { 
-        
-        // Generate a random index for the filenames 
-        const randomIndex = Math.floor(Math.random() * props.filenames.length); 
-
-        // Then generate a random left and top position 
-        const randomLeft = Math.floor(Math.random() * 100); 
-        const randomTop = Math.floor(Math.random() * 100); 
-
-        // Then generate a random size 
-        const randomSize = Math.floor(Math.random() * 100); 
-
-        // Then generate a random velocity 
-        const randomVelocity = Math.floor(Math.random() * 10); 
-
-        // Then generate the metaball props 
-        const metaballProps: MetaballProps = {
-            spotFile: props.filenames[randomIndex], 
-            left: randomLeft, 
-            top: randomTop, 
-            size: randomSize, 
-            initialLeft: randomLeft, 
-            initialTop: randomTop, 
-            velocity: randomVelocity
-        }
-
-        // Then push the metaball props onto the list 
-        metaballPropsList.push(metaballProps); 
-
-    }
-
-    return metaballPropsList;
-
-    // return [
-    //     {spotFile: "/cowspots/CowSpot1.svg", left: 30, top: 50, size: 200, initialLeft: 30, initialTop: 50, velocity: 1},
-    //     {spotFile: "/cowspots/CowSpot1.svg", left: 50, top: 50, size: 200, initialLeft: 50, initialTop: 50, velocity: -1}
-    // ];
+    velocityX: number, 
+    velocityY: number
 }
 
 // Define a function that updates the metaball (moves it on the screen)
 function updateMetaballs(metaballPropsList: MetaballProps[]) {
     return metaballPropsList.map((metaballProps) => {
 
-        if (metaballProps.left > metaballProps.initialLeft + 10 || metaballProps.left < metaballProps.initialLeft - 10) {
-            metaballProps.velocity = -1 * metaballProps.velocity; 
+        // Check for horizontal wall collisions (left and right)
+        if (metaballProps.left > 100 || metaballProps.left < 0) {
+            metaballProps.velocityX = -1.0 * metaballProps.velocityX; // reverse and slow down velocity
         }
 
-        // Then update position 
-        metaballProps.left += metaballProps.velocity * (1/50);
+        // Check for vertical wall collisions (top and bottom)
+        if (metaballProps.top > 100 || metaballProps.top < 0) {
+            metaballProps.velocityY = -1.0 * metaballProps.velocityY; // reverse and slow down velocity
+        }
+
+        // Update positions
+        metaballProps.left += metaballProps.velocityX * (1/50);
+        metaballProps.top += metaballProps.velocityY * (1/50);
+
+        if (Math.abs(metaballProps.velocityX) <= 0.1 || Math.abs(metaballProps.velocityY) <= 0.1) {
+            metaballProps.velocityX = Math.random() * 3 + 0.1; 
+            metaballProps.velocityY = Math.random() * 3 + 0.1;
+        }
 
         return metaballProps;
     });
@@ -111,31 +77,32 @@ const Metaball: React.FC<MetaballProps> = (props) => {
 
     return (
         <div style={style}>
-            <img src={props.spotFile} />
+            <img src={props.spotFile} draggable={false} />
         </div>
     );
 }
 
+
+
+interface MetaballsProps { 
+    initialMetaballPropsList: MetaballProps[]; 
+}
+
 // Parent component to be rendered 
-const Metaballs: React.FC<MetaballGeneratorProps> = (props) => {
+const Metaballs: React.FC<MetaballsProps> = ({ initialMetaballPropsList }) => {
 
     // Set up initial state for metaballs
-    const [metaballPropsList, setMetaballPropsList] = useState<MetaballProps[]>(generateInitialMetaballs(props));
+    const [metaballPropsList, setMetaballPropsList] = useState<MetaballProps[]>(initialMetaballPropsList);
 
     // Then add the useEffect hook to set an interval to update everyone's properties 
     // This function is called when the component is mounted. 
     useEffect(() => {
 
-        // I expect this to be ran once, when the component is mounted.
-        console.log("Inside metaball generator!");
-        console.log("Props: ", props.filenames);
-
-        // Also display initial props in console
-        console.log("Initial metaball props: ", metaballPropsList);
+        console.log("Prop list: ", metaballPropsList);
 
         const interval = setInterval(() => {
             setMetaballPropsList(
-                updateMetaballs(metaballPropsList)
+                (prevPropsList) => updateMetaballs(prevPropsList)
             );
         }, Math.floor(1/60 * 1000));
 
@@ -147,7 +114,6 @@ const Metaballs: React.FC<MetaballGeneratorProps> = (props) => {
     return (
         <div className={styles.outerMetaBallsContainer}>
             <div className={styles.innerMetaBallsContainer}>
-                
                 {metaballPropsList.map((metaballProps, index) => {
                     return (
                         <Metaball key={index} {...metaballProps}/>
@@ -160,6 +126,3 @@ const Metaballs: React.FC<MetaballGeneratorProps> = (props) => {
 
 // Default export for the functional component 
 export default Metaballs;
-
-// Named export for the interface 
-export type { MetaballGeneratorProps }; 
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
-import Metaballs from './components/MetaBallsAttempt2'
-import { MetaballGeneratorProps } from './components/MetaBallsAttempt2';
+import Metaballs, { MetaballGeneratorProps, MetaballProps } from './components/MetaBallsAttempt2';
 import fs from 'fs'; 
 import path from 'path'; 
 
@@ -22,22 +21,77 @@ export function getCowSpotFiles(folder: string) {
   return svgFiles;
 }
 
+// Define a function that generates initial metaball props
+// This is called in the parent page to ensure that props are agreed upon 
+// between server and client 
+function createInitialMetaballs(props: MetaballGeneratorProps): MetaballProps[] {
+
+  let metaballPropsList: MetaballProps[] = []; 
+
+  // Decompose the props, generate a metaball for each. 
+  for (let i = 0; i < props.numMetaballs; i++) { 
+      
+      // Generate a random index for the filenames 
+      const randomIndex = Math.floor(Math.random() * props.filenames.length); 
+
+      // Then generate a random left and top position 
+      const randomLeft = Math.floor(Math.random() * 100 - 50); 
+      const randomTop = Math.floor(Math.random() * 100 - 50); 
+
+      // Then generate a random size 
+      const randomSize = Math.random() * 700 + 400; 
+
+      // Then generate a random velocity 
+      const randomVelocityX = Math.random() * 3 + 0.1; 
+      const randomVelocityY = Math.random() * 3 + 0.1;
+
+      console.log("Generating random velocity x", randomVelocityX);
+      console.log("Generating random velocity y", randomVelocityY);
+
+      // Then generate the metaball props 
+      const metaballProps: MetaballProps = {
+          spotFile: props.filenames[randomIndex], 
+          left: randomLeft, 
+          top: randomTop, 
+          size: randomSize, 
+          initialLeft: randomLeft, 
+          initialTop: randomTop, 
+          velocityX: randomVelocityX, 
+          velocityY: randomVelocityY, 
+      }
+
+      // Then push the metaball props onto the list 
+      metaballPropsList.push(metaballProps); 
+
+  }
+
+  return metaballPropsList;
+
+  // return [
+  //     {spotFile: "/cowspots/CowSpot1.svg", left: 30, top: 50, size: 200, initialLeft: 30, initialTop: 50, velocity: 1},
+  //     {spotFile: "/cowspots/CowSpot1.svg", left: 50, top: 50, size: 200, initialLeft: 50, initialTop: 50, velocity: -1}
+  // ];
+}
+
 
 export default async function Home() {
 
-  // Before we call the Metaballs component below, we need 
-  // to generate some props for it. 
-  const propsForMetaballs: MetaballGeneratorProps = {
-    filenames: getCowSpotFiles(cowSpotsFolder), 
-    numMetaballs: 1, 
-    generationOrder: 'random'
+  const metaballGeneratorProps: MetaballGeneratorProps = {
+      filenames: getCowSpotFiles(cowSpotsFolder), 
+      numMetaballs: 15, 
+      generationOrder: 'random'
   }
 
+  const metaballPropsList : MetaballProps[] = createInitialMetaballs(metaballGeneratorProps);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Metaballs {...propsForMetaballs}/>
-      <div className="overlay-content"> 
-        <h1 className="text-4xl font-bold text-center">Test🚀</h1>
+    <main className="relative w-full h-screen">
+      <Metaballs initialMetaballPropsList={metaballPropsList} />
+      <div className="overlay-content absolute inset-0 flex justify-center items-center text-center "> 
+        <div className="max-w-full max-h-fullbg-opacity-100 bg-white">
+          <h1 className="text-4xl font-bold">Welcome to Cow2k</h1>
+          <h2 className="text-3xl font-bold">A one of a kind hobby rocket organization.</h2>
+        </div>
       </div> 
     </main>
   )
